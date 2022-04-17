806a758b975a177d0f76c92047d8477e2407f90c
saroj adhikari <adhikarisaroj291@gmail.com>
2022-04-18 00:17:28 +0545

    change in code

--- Roomfinder/app/src/main/java/com/example/roomfinder/MainActivity.java
+++ Roomfinder/app/src/main/java/com/example/roomfinder/MainActivity.java
@@ -2,13 +2,57 @@ package com.example.roomfinder;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button button;
+    private Button btnR;
+    public void openlogin()
+    {
+        Intent intent = new Intent(this, Login.class);
+        startActivity(intent);
+    }
+
+    public void openReg()
+    {
+        Intent intent = new Intent(this, Register.class);
+        startActivity(intent);
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+
+
+        button = findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                openlogin();
+            }
+
+
+        });
+
+
+        btnR = findViewById(R.id.btnR);
+        btnR.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                openReg();
+            }
+        });
+
+
+
+
+
     }
 }
